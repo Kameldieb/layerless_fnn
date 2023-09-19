aea46e4e307f0f3245f32e591d8ba9112f259f48
Moritz <71906094+Kameldieb@users.noreply.github.com>
2023-09-19 23:54:57 +0200

    Update main.cu

--- main.cu
+++ main.cu
@@ -36,7 +36,7 @@ int main(void) {
 	loader.load_mnist();
 	loader.set_elastic_augmentation(8, 8);
 
-	Net net_host(loader.in_size, 512, loader.out_size, 1000, 0.5, 0.1, LEAKY_RELU);
+	Net net_host(loader.in_size, 1024, loader.out_size, 7680, 0.5, 0.1, LEAKY_RELU);
 	Net *net;
 	cudaMallocManaged(&net, sizeof(Net));
 	std::memcpy(net, &net_host, sizeof(Net));
@@ -45,7 +45,7 @@ int main(void) {
 	for (int i = 0; i < 1000; ++i) {
 		if (i % 100 == 0 and i > 0) net->print_connections();
 		std::cout << "training nr. " << i << std::flush;
-		train_and_restructure(net, loader, i > 190 ? 10 : 20, 10000, 1, 1.0/(i+100));
+		train_and_restructure(net, loader, 200, 10000, 1, 1.0/(i+100));
 		std::cout <<  " done\n";
 		std::cout << "test set correct: " << test(net, loader) << '\n';
 	}
